0ea10ac534fb4120df3ea2547c4b3a93942085ad
kelvin <kelvin950>
2022-12-26 01:31:32 +0000

    creating request controller

--- src/controllers/shopController.ts
+++ src/controllers/shopController.ts
@@ -1,5 +1,7 @@
 import Shop  from "@models/Shop";
 import BadRequestError from "@errors/BadRequestError";
+import User from '@models/User';
+import RequestModel from '@models/Request';
 import{Response , Request} from 'express'
 
 export const createShop = async (req:Request,res:Response)=> {
@@ -32,8 +34,24 @@ export const createShop = async (req:Request,res:Response)=> {
 export const requestForShop =async (req:Request, res:Response) => {
         
 
+        
+    const user =  await User.findById(req.user.id);
+
+     if(user?.shop)
+                throw new BadRequestError("Already owns a shop");
+
     
-      
+    const isRequest = await RequestModel.find({User:req.user.id}) ;
+
+    if(isRequest)
+        throw new BadRequestError("You have already asked for a shop");
+    
+    const request=  await RequestModel.create({
+        user:req.user.id 
+    }) ;
+
+    await request.save();
+
 
 }
 
