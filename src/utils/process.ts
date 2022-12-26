aae33c120dd88e51433051d0ab397f4f360d6091
kelvin <kelvin950>
2022-12-26 02:14:39 +0000

    integrating sendgrid

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -1,6 +1,20 @@
-const processes = async()=>{
+import * as sgMail from '@sendgrid/mail'
+export  const processes = async(job:any)=>{
 
-
-    
-
-}
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+// const msg = {
+//   to: "test@example.com", // Change to your recipient
+//   from: "test@example.com", // Change to your verified sender
+//   subject: "Sending with SendGrid is Fun",
+//   text: "and easy to do anywhere, even with Node.js",
+//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+// };
+sgMail
+  .send(job.data)
+  .then(() => {
+    console.log("Email sent");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+} ;
--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -1,10 +1,11 @@
 import bull from "bull";
-
+import {processes} from './process';
 
 const emailQueue =  new bull("email",{
     redis:process.env.redis_url!
 }) ;
 
+emailQueue.process(processes);
 
 const sendEmail =(data:any)=>{
 
