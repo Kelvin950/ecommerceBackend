485239ea0061125a9861c0e4b7a2097a408947dd
kelvin <kelvin950>
2023-01-26 23:50:45 +0000

    exporting middlewares

--- src/routes/productRoute/create.ts
+++ src/routes/productRoute/create.ts
@@ -2,11 +2,12 @@ import  express from 'express' ;
 import {createProduct ,deleteProduct ,deleteProducts , updateProduct ,getProduct ,getProducts} from '@controllers/productController' ;
 import  validationError from '@middleWares/validationError'
 import {body} from 'express-validator';
+import isAuth from '@middleWares/ISAuth'
 const router =  express.Router() ;
 
 router.route("/product")
 .post( [body("title").notEmpty().
-trim() , body("price").notEmpty().isNumeric().trim() , body("description").notEmpty().isLength({min:15}) , body("image").notEmpty()] , validationError ,createProduct)
+trim() , body("price").notEmpty().isNumeric().trim() , body("description").notEmpty().isLength({min:15}) , body("image").notEmpty()] , validationError,isAuth.isAuth ,createProduct)
 .delete(deleteProducts)
 .get(getProducts)
 
